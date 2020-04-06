d4c0e5716117d36a00c932123e89e7fdae561fff
darkvictor13 <victoralmeida2001@hotmail.com>
2020-04-06 19:43:43 -0300

    Excluindo fedora

--- 1-teste-bluetooth/1-teste-bluetooth.ino
+++ 1-teste-bluetooth/1-teste-bluetooth.ino
@@ -1,4 +1,4 @@
-ard#include "BluetoothSerial.h"
+#include "BluetoothSerial.h"
 
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
